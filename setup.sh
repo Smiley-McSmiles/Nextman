88b48fd51277175264e76c31908bce686ea1e5ae
WOOSAH <31331596+Smiley-McSmiles@users.noreply.github.com>
2024-11-13 09:42:28 -0600

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -5,17 +5,23 @@ if ! HasSudo; then
 fi
 
 DIRECTORY=$(cd `dirname $0` && pwd)
+logFile="nextman_install.log"
 defaultDir="/var/www/nextcloud"
+dataDir="$defaultDir/data"
+nextmanDir="/etc/nextman"
+nextmanConf="$nextmanDir/nextman.conf"
 defaultPort="8090"
 defaultDBPort="3306"
-zipFileLink="https://download.nextcloud.com/server/releases/latest.zip"
+nextcloudLatestArchive="https://download.nextcloud.com/server/releases/latest.zip"
 apacheNcConf="configs/nextcloud.conf"
 nextcloudCronService="configs/nextcloudcron.service"
 nextcloudCronTimer="configs/nextcloudcron.timer"
 nextcloudUpdate="scripts/nextcloud-update.sh"
 scanFiles="scripts/scanFiles.sh"
 hasSELinux=false
-apacheLocation=""
+ncAdmin=""
+ncPass=""
+apacheDir=""
 apacheUser=""
 
 Update()
@@ -27,7 +33,7 @@ Update()
 InstallDependencies()
 {
 	packagesNeededRHEL="cronie fastlz liblzf libmemcached-awesome httpd php php-fpm php-cli php-mysqlnd php-gd php-xml php-mbstring php-json php-curl php-zip php-bcmath php-gmp php-intl php-ldap php-pecl-apcu php-pecl-igbinary php-pecl-imagick php-pecl-memcached php-pecl-msgpack php-pecl-redis5 php-smbclient php-process php-imagick php-redis php-opcache redis mariadb mariadb-server unzip curl wget bash-completion policycoreutils-python-utils mlocate bzip2"
-	packagesNeededDebian="cron lz4 liblzf-dev libmemcached-dev apache2 php php-fpm php-cli php-mysql php-gd php-xml php-mbstring php-json php-curl php-zip php-bcmath php-gmp php-intl php-ldap php-apcu php-igbinary php-imagick php-memcached php-msgpack php-redis php-smbclient php-process php-imagick php-redis php-opcache redis-server mariadb-server unzip curl wget bash-completion policykit-1 mlocate bzip2"
+	packagesNeededDebian="cron lz4 liblzf-dev libmemcached-dev apache2 php php-fpm php-cli php-mysql php-gd php-xml php-mbstring php-json php-curl php-zip php-bcmath php-gmp php-intl php-ldap php-apcu php-igbinary php-imagick php-memcached php-msgpack php-redis php-smbclient php-imagick php-redis php-opcache redis-server mariadb-server unzip curl wget bash-completion policykit-1 plocate bzip2"
 	packagesNeededArch="cronie lz4 lib32-libmemcached apache php php-fpm php-cli php-gd php-mbstring php-json php-curl php-zip php-bcmath php-gmp php-intl php-apcu php-igbinary php-imagick php-memcached php-msgpack php-redis php-smbclient php-imagick php-redis php-opcache redis mariadb mariadb-clients unzip curl wget bash-completion polkit mlocate bzip2"
 	packagesNeededOpenSuse="cron fastlz liblzf1 libmemcached apache2 php php-fpm php-cli php-mysql php-gd php-xml php-mbstring php-json php-curl php-zip php-bcmath php-gmp php-intl php-ldap php7-apcu php7-igbinary php7-imagick php7-memcached php7-msgpack php7-redis php-smbclient php-process php-imagick php-redis php-opcache redis mariadb mariadb-client unzip curl wget bash-completion policycoreutils mlocate bzip2"
 
@@ -38,6 +44,7 @@ InstallDependencies()
 		crbOrPowertools=
 		osDetected=true
 		echo "> ID=$ID"
+		Log "SETUP | ID = $ID" $logFile
 		
 		if [[ $ID_LIKE == .*"rhel".* ]] || [[ $ID == "rhel" ]]; then
 			ID=rhel
@@ -56,22 +63,22 @@ InstallDependencies()
 			case "$ID" in
 				fedora)	dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 						dnf install $packagesNeededRHEL -y
-						apacheLocation="/etc/httpd/conf.d" 
+						apacheDir="/etc/httpd/conf.d" 
 						hasSELinux=true ;;
 				rhel)	dnf install epel-release -y
 						dnf config-manager --set-enabled $crbOrPowertools
 						dnf install --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm -y https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm -y
 						dnf install $packagesNeededRHEL -y
-						apacheLocation="/etc/httpd/conf.d"
+						apacheDir="/etc/httpd/conf.d"
 						hasSELinux=true ;;
 				debian|ubuntu|linuxmint|elementary)
 						apt install $packagesNeededDebian -y
-						apacheLocation="/etc/apache2/sites-available";;
+						apacheDir="/etc/apache2/sites-available";;
 				arch|endeavouros|manjaro)
 						pacman -Syu $packagesNeededArch
-						apacheLocation="/etc/httpd/sites-available" ;;
+						apacheDir="/etc/httpd/sites-available" ;;
 				opensuse*)		zypper install $packagesNeededOpenSuse
-						apacheLocation="/etc/httpd/conf.d"
+						apacheDir="/etc/httpd/conf.d"
 						hasSELinux=true ;;
 			esac
 	else
@@ -88,8 +95,10 @@ InstallDependencies()
 	fi
 	if id -u apache &>/dev/null; then
 		apacheUser="apache"
+		SetVar apacheUser "$apacheUser" "$nextmanConf" bash user
 	elif id -u www-data &>/dev/null; then
 		apacheUser="www-data"
+		SetVar apacheUser "$apacheUser" "$nextmanConf" bash user
 	else
 		echo "+---------------------------------------------------+"
 		echo "|               ******WARNING******                 |"
@@ -99,38 +108,42 @@ InstallDependencies()
 		echo "+---------------------------------------------------+"
 		read -p "Press ENTER to acknowledge and continue" ENTER
 	fi
+	Log "SETUP | apacheUser = $apacheUser" $logFile
 }
 
 ConfigureFirewall() {
-    echo "> Configuring firewall rules..."
+	echo "> Configuring firewall rules..."
 
-    if command -v firewall-cmd &>/dev/null; then
-        echo "> Detected firewalld. Configuring rules..."
-        firewall-cmd --permanent --add-service=http
-        firewall-cmd --permanent --add-port=$defaultPort/tcp
-        firewall-cmd --permanent --add-port=$defaultDBPort/tcp
-        firewall-cmd --reload
+	if command -v firewall-cmd &>/dev/null; then
+		echo "> Detected firewalld. Configuring rules..."
+		firewall-cmd --permanent --add-service=http
+		firewall-cmd --permanent --add-port=$defaultPort/tcp
+		firewall-cmd --permanent --add-port=$defaultDBPort/tcp
+		firewall-cmd --reload
+		Log "SETUP | firewall = FirewallD" $logFile
+	elif command -v ufw &>/dev/null; then
+		echo "> Detected UFW. Configuring rules..."
+		ufw allow http
+		ufw allow $defaultPort/tcp
+		ufw allow $defaultDBPort/tcp
+		ufw reload
+		Log "SETUP | firewall = UFW" $logFile
+	else
+		echo "+-------------------------------------------------+"
+		echo "|               ******WARNING******               |"
+		echo "|        Firewall service not detected.           |"
+		echo "| Please manually configure the following ports:  |"
+		echo "|   - HTTP (port 80)                              |"
+		echo "|   - Nextcloud (port $defaultPort)                       |"
+		echo "|   - MariaDB (port $defaultDBPort)                         |"
+		echo "+-------------------------------------------------+"
+		read -p "Press ENTER to acknowledge and continue" ENTER
+	fi
 
-    elif command -v ufw &>/dev/null; then
-        echo "> Detected UFW. Configuring rules..."
-        ufw allow http
-        ufw allow $defaultPort/tcp
-        ufw allow $defaultDBPort/tcp
-        ufw reload
+	SetVar defaultPort "$defaultPort" "$nextmanConf" bash num
+	SetVar defaultDBPort "$defaultDBPort" "$nextmanConf" bash num
 
-    else
-        echo "+-------------------------------------------------+"
-        echo "|               ******WARNING******               |"
-        echo "|        Firewall service not detected.           |"
-        echo "| Please manually configure the following ports:  |"
-        echo "|   - HTTP (port 80)                              |"
-        echo "|   - Nextcloud (port $defaultPort)                       |"
-        echo "|   - MariaDB (port $defaultDBPort)                         |"
-        echo "+-------------------------------------------------+"
-        read -p "Press ENTER to acknowledge and continue" ENTER
-    fi
-    
-    if $hasSELinux; then
+if $hasSELinux; then
 		semanage fcontext -a -t httpd_sys_rw_content_t '$defaultDir/config(/.*)?'
 		semanage fcontext -a -t httpd_sys_rw_content_t '$defaultDir/apps(/.*)?'
 		semanage fcontext -a -t httpd_sys_rw_content_t '$defaultDir/.htaccess'
@@ -138,67 +151,37 @@ ConfigureFirewall() {
 		semanage fcontext -a -t httpd_sys_rw_content_t '$defaultDir/3rdparty/aws/aws-sdk-php/src/data/logs(/.*)?'
 		restorecon -R '$defaultDir/'
 		setsebool -P httpd_can_network_connect on
-		chcon -Rt httpd_sys_rw_content_t $defaultDir/data
-		semanage fcontext -a -t httpd_sys_rw_content_t  "$defaultDir/data(/.*)?"
-		semanage fcontext -m -t httpd_sys_rw_content_t  "$defaultDir/data(/.*)?"
+		chcon -Rt httpd_sys_rw_content_t $dataDir
+		semanage fcontext -a -t httpd_sys_rw_content_t  "$dataDir(/.*)?"
+		semanage fcontext -m -t httpd_sys_rw_content_t  "$dataDir(/.*)?"
+		Log "SETUP | SELinux is enabled" $logFile
 	fi
-
 }
 
 # PHP Configurations
 ConfigurePHP() {
-    echo "> Configuring PHP settings..."
+	echo "> Configuring PHP settings..."
 
-    # Detect PHP-FPM configuration directory based on distribution
-    if [[ -d "/etc/php-fpm.d" ]]; then
-        phpFpmDir="/etc/php-fpm.d"
-        phpIni="/etc/php.ini"
-        phpFpmConf="$phpFpmDir/www.conf"
-        phpUser="apache"
-        phpGroup="apache"
-        
-    elif [[ -d "/etc/php/7.4/fpm/pool.d" ]]; then
-        phpFpmDir="/etc/php/7.4/fpm/pool.d"
-        phpIni="/etc/php/7.4/fpm/php.ini"
-        phpFpmConf="$phpFpmDir/www.conf"
-        phpUser="www-data"
-        phpGroup="www-data"
-        
-    elif [[ -d "/etc/php/8.0/fpm/pool.d" ]]; then
-        phpFpmDir="/etc/php/8.0/fpm/pool.d"
-        phpIni="/etc/php/8.0/fpm/php.ini"
-        phpFpmConf="$phpFpmDir/www.conf"
-        phpUser="www-data"
-        phpGroup="www-data"
+	# Detect PHP-FPM configuration directory based on distribution
+	phpIni=$(find /etc/ -name "php.ini" -type f)
+	phpFpmConf=$(find /etc/ -name "www.conf" -type f)
+	SetVar phpIni "$phpIni" "$nextmanConf" bash string
+	SetVar phpFpmConf "$phpFpmConf" "$nextmanConf" bash string
+	Log "SETUP | phpIni = $phpIni" $logFile
+	Log "SETUP | phpFpmConf = $phpFpmConf" $logFile
+	
+	# Configure php.ini settings
+	echo "> Modifying PHP memory limit in $phpIni..."
+	sed -ri "s|memory_limit =.*|memory_limit = 512M|g" "$phpIni"
 
-    elif [[ -d "/etc/php-fpm.d" ]]; then
-        phpFpmDir="/etc/php-fpm.d"
-        phpIni="/etc/php.ini"
-        phpFpmConf="$phpFpmDir/www.conf"
-        phpUser="nginx"
-        phpGroup="nginx"
-    else
-        echo "+-------------------------------------------------+"
-        echo "|               ******WARNING******               |"
-        echo "|         PHP-FPM configuration not found.        |"
-        echo "|   Please manually locate and configure PHP.     |"
-        echo "+-------------------------------------------------+"
-        read -p "Press ENTER to acknowledge and continue" ENTER
-        return
-    fi
-
-    # Configure php.ini settings
-    echo "> Modifying PHP memory limit in $phpIni..."
-    sed -ri "s|memory_limit =.*|memory_limit = 512M|g" "$phpIni"
-
-    # Configure PHP-FPM settings
-    echo "> Configuring PHP-FPM settings in $phpFpmConf..."
-    sed -ri "s|^user =.*|user = $phpUser|g" "$phpFpmConf"
-    sed -ri "s|^group =.*|group = $phpGroup|g" "$phpFpmConf"
-    sed -ri "s|^listen =.*|listen = /run/php-fpm/www.sock|g" "$phpFpmConf"
-    sed -ri "s|^listen.owner =.*|listen.owner = $phpUser|g" "$phpFpmConf"
-    sed -ri "s|^listen.group =.*|listen.group = $phpGroup|g" "$phpFpmConf"
-    echo "php_value[opcache.interned_strings_buffer] = 8" >> "$phpFpmConf"
+	# Configure PHP-FPM settings
+	echo "> Configuring PHP-FPM settings in $phpFpmConf..."
+	sed -ri "s|^user =.*|user = $apacheUser|g" "$phpFpmConf"
+	sed -ri "s|^group =.*|group = $apacheUser|g" "$phpFpmConf"
+	sed -ri "s|^listen =.*|listen = /run/php-fpm/www.sock|g" "$phpFpmConf"
+	sed -ri "s|^listen.owner =.*|listen.owner = $apacheUser|g" "$phpFpmConf"
+	sed -ri "s|^listen.group =.*|listen.group = $apacheUser|g" "$phpFpmConf"
+	echo "php_value[opcache.interned_strings_buffer] = 8" >> "$phpFpmConf"
 }
 
 Setup()
@@ -215,41 +198,69 @@ Setup()
 	PromptUser str "Please enter the desired admin account password for Nextcloud : " 0 0 "password"
 	ncPass="$promptResult"
 	echo
+	
+	if PromptUser yN "Would you like to change the default Nextcloud port from 8090?" 0 0 "y/N"; then
+		PromptUser num "Enter a valid port" 1024 65535 "1024-65535"
+		defaultPort=$promptResult
+		echo "> Changing Nextcloud port to $defaultPort"
+	else
+		echo "> Keeping Nextcloud on port 8090..."
+	fi
+
+	if PromptUser yN "Would you like to change the default data directory from $dataDir?" 0 0 "y/N"; then
+		PromptUser dir "Enter an existing directory" 0 0 "/path/to/data"
+		dataDir=$promptResult
+		echo "> Changing $defaultDir/data to $dataDir..."
+	else
+		echo "> Keeping $dataDir as data directory..."
+	fi
+
+	# Install Nextman
+	mkdir -p "$nextmanDir"
+	cp -f "$nextcloudCronService" "$nextcloudCronTimer" /usr/lib/systemd/system/nextcloudcron.service
+	SetVar mariadbUser "$mariadbUser" "$nextmanConf" bash string
+	SetVar mariadbPass "$mariadbPass" "$nextmanConf" bash string
+	SetVar ncAdmin "$ncAdmin" "$nextmanConf" bash string
+	SetVar ncPass "$ncPass" "$nextmanConf" bash string
+	SetVar defaultPort "$defaultPort" "$nextmanConf" bash number
+	SetVar dataDir "$dataDir" "$nextmanConf" bash string
+	Log "SETUP | mariadbUser = $mariadbUser" $logFile
+	Log "SETUP | mariadbPass = $mariadbPass" $logFile
+	Log "SETUP | ncAdmin = $ncAdmin" $logFile
+	Log "SETUP | ncPass = $ncPass" $logFile
+	Log "SETUP | defaultPort = $defaultPort" $logFile
+	Log "SETUP | dataDir = $dataDir" $logFile
 
 	# Install Dependencies
 	InstallDependencies
 
 	# Install Nextcloud
-	wget $zipFileLink
+	wget $nextcloudLatestArchive
 	unzip latest.zip -d /var/www/
-	mkdir -p $defaultDir/data
-	chown -Rf apache:apache $defaultDir
+	mkdir -p $dataDir
+	chown -Rf $apacheUser:apacheUser $defaultDir
 	chmod -Rf 770 $defaultDir
-	cp -f scripts/scanFiles.sh scripts/nextcloud-update.sh $defaultDir/data/
-	chmod +x $defaultDir/data/*.sh
-	cp -f "$apacheNcConf" > /etc/httpd/conf.d/
-
+	cp -f "$apacheNcConf" $apacheDir/
+	
 	ConfigureFirewall
 	ConfigurePHP
 
 	## Enable Services
-	echo "$nextcloudCronService" > /usr/lib/systemd/system/nextcloudcron.service
-	echo "$nextcloudCronTimer" > /usr/lib/systemd/system/nextcloudcron.timer
 	systemctl daemon-reload
 	systemctl enable --now php-fpm httpd mariadb redis nextcloudcron.service nextcloudcron.timer
 
 	## MariaDB Setup
 	mysql_secure_installation
-
 	mysql -e "CREATE USER '${mariadbUser}'@'localhost' IDENTIFIED BY '${mariadbPass}';"
 	mysql -e "CREATE DATABASE IF NOT EXISTS nextcloud CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
 	mysql -e "GRANT ALL PRIVILEGES on nextcloud.* to '${mariadbUser}'@'localhost';"
 
 	# Install Nextcloud via occ
-	chmod -Rf 777 $defaultDir
+	chmod -Rf 770 $defaultDir
+	chown -Rf $apacheUser:$apacheUser $defaultDir
 	cd $defaultDir
 	sudo -u $apacheUser php occ maintenance:install --database='mysql' --database-name='nextcloud' --database-user='${mariadbUser}' --database-pass='${mariadbPass}' --admin-user='${ncAdmin}' --admin-pass='${ncPass}'
-	cd ~; chmod -Rf 770 $defaultDir
+	cd -
 
 	# Edit $defaultDir/config/config.php
 	echo "We must edit the variable 'trusted_domains' in the Nextcloud config.php."
@@ -263,7 +274,7 @@ Setup()
 		0 => 'localhost',
 		1 => '127.0.0.1',
 		2 => 'cloud.YourDomain.com',
-		3 => '192.168.1.11:8090',
+		3 => '192.168.1.11:${defaultPort}',
 	  ),
 	  'trusted_proxies' => ['127.0.0.1'],
 	  'overwritehost' => 'cloud.YourDomain.com',
@@ -280,10 +291,12 @@ Setup()
 	echo
 	echo
 	echo "All done. Please navigate to http://yourdomain:$defaultPort"
+	echo "User: $ncAdmin"
+	echo "Pass: $ncPass"
 	echo "It is recommended to use caddy to make a reverse proxy to host Nextcloud with https (secure)"
 	echo
 	echo "To change the user data directory please edit $defaultDir/config/config.php"
-	echo "Edit line 'datadirectory' => '$defaultDir/data',"
+	echo "Edit line 'datadirectory' => '$dataDir',"
 	echo "Then run:"
 	echo '----------------------------------------------------
 sudo chcon -Rt httpd_sys_rw_content_t /path/to/new/nextcloud/data
