9e49a8926503d12579133108252a38e37414c1e9
WOOSAH <31331596+Smiley-McSmiles@users.noreply.github.com>
2025-02-18 19:46:55 -0600

    Update nextcloud-update.sh

--- scripts/nextcloud-update.sh
+++ scripts/nextcloud-update.sh
@@ -3,12 +3,11 @@
 echo "Turning off security:"
 sudo setsebool httpd_unified on
 
-read -p "Press enter to turn security back on once update is finished" ENTER
-
-sudo setsebool -P httpd_unified off
-
-cd /var/www/nextcloud
+cd /var/www/html/nextcloud
 sudo chmod +x occ
+sudo -u apache ./occ upgrade
 sudo -u apache ./occ maintenance:repair --include-expensive
 sudo -u apache ./occ db:add-missing-indices
 cd -
+
+sudo setsebool -P httpd_unified off
