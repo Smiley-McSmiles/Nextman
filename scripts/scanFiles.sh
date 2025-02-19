75fd89e43b076f631196eb1f6386a1040b16aa26
WOOSAH <31331596+Smiley-McSmiles@users.noreply.github.com>
2025-02-18 19:47:18 -0600

    Update scanFiles.sh

--- scripts/scanFiles.sh
+++ scripts/scanFiles.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo -u apache php /var/www/nextcloud/occ files:scan --all
+sudo chown -Rf apache:apache /shared/nextcloud/data
+sudo chmod -Rf 770 /shared/nextcloud/data
+sudo -u apache php /var/www/html/nextcloud/occ files:scan --all
